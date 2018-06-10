a65d5f71d3807034e7915b3d9632c6673278169b
Unknown <shantic@gmail.com>
2018-06-10 18:56:45 -0500

    autostart.sh: Run Kodi as default

--- packages/sx05re/sx05re/autostart.sh
+++ packages/sx05re/sx05re/autostart.sh
@@ -9,20 +9,20 @@ rm -rf /storage/.cache/cores/*
 DEFE=$(sed -n 's|\s*<string name="Sx05RE_BOOT" value="\(.*\)" />|\1|p' /storage/.emulationstation/es_settings.cfg)
 
 case "$DEFE" in
-"Kodi")
-	rm -rf /var/lock/start.retro
-	rm -rf /var/lock/start.games
-	touch  /var/lock/start.kodi
-	;;
 "Retroarch")
 	rm -rf /var/lock/start.kodi
 	rm -rf /var/lock/start.games
 	touch /var/lock/start.retro
 	systemctl start retroarch
 	;;
-*)
+"Emulationstation")
 	rm -rf /var/lock/start.kodi
 	rm -rf /var/lock/start.retro
 	/usr/bin/startfe.sh &
+	;;
+*)
+	rm -rf /var/lock/start.retro
+	rm -rf /var/lock/start.games
+	touch  /var/lock/start.kodi
 	;;
 esac
