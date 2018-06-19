9f8acbaed7dc629ca6ea8c77db49e5706d39fcf0
Unknown <shantic@gmail.com>
2018-06-19 00:35:43 -0500

    SDL2: Bump to 2.0.8

--- packages/multimedia/SDL2/package.mk
+++ packages/multimedia/SDL2/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="SDL2"
-PKG_VERSION="2.0.7"
+PKG_VERSION="2.0.8"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="https://www.libsdl.org/"
