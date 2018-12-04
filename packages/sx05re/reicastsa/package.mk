dcc78c187dffceb27e271dce1fdccbedbc7e4db4
Unknown <shantic@gmail.com>
2018-12-04 13:30:22 -0600

    Reicast: Bump to 3ab2eef

--- packages/sx05re/reicastsa/package.mk
+++ packages/sx05re/reicastsa/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="reicastsa"
-PKG_VERSION="4b24d22"
+PKG_VERSION="3ab2eef"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/reicast/reicast-emulator"
