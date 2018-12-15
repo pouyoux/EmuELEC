bb548a9838f368548d1499ed3379a8d0c2288865
Unknown <shantic@gmail.com>
2018-12-15 02:13:24 -0600

    ES: Fix Addon

--- packages/sx05re/sx05re/bin/startfe.sh
+++ packages/sx05re/sx05re/bin/startfe.sh
@@ -3,10 +3,10 @@
     touch /var/lock/start.games
     #systemctl start emustation
     cd /usr/bin/    
+    /usr/bin/emustation-config 
+    /usr/bin/emulationstation &
     rm /var/lock/start.kodi
     systemctl stop kodi
-    /usr/bin/emustation-config
-    /usr/bin/emulationstation
 
 
 
