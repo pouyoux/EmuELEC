4b7462a4bf1a29610eaa918d08c902325dd5d795
Unknown <shantic@gmail.com>
2019-01-09 21:12:01 -0600

    Freeimage: bump to 3180

--- packages/sx05re/tools/freeimage/package.mk
+++ packages/sx05re/tools/freeimage/package.mk
@@ -1,40 +1,17 @@
-################################################################################
-#      This file is part of LibreELEC - http://www.libreelec.tv
-#      Copyright (C) 2016 Team LibreELEC
-#
-#  LibreELEC is free software: you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation, either version 2 of the License, or
-#  (at your option) any later version.
-#
-#  LibreELEC is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License
-#  along with LibreELEC.  If not, see <http://www.gnu.org/licenses/>.
-################################################################################
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2018-present 5schatten (https://github.com/5schatten)
 
 PKG_NAME="freeimage"
-PKG_VERSION="3170"
-PKG_REV="1"
-PKG_ARCH="any"
+PKG_VERSION="3180"
+PKG_SHA256="f41379682f9ada94ea7b34fe86bf9ee00935a3147be41b6569c9605a53e438fd"
 PKG_LICENSE="GPLv3"
 PKG_SITE="http://freeimage.sourceforge.net/"
-PKG_URL="http://downloads.sourceforge.net/freeimage/FreeImage$PKG_VERSION.zip"
+PKG_URL="$SOURCEFORGE_SRC/$PKG_NAME/FreeImage$PKG_VERSION.zip"
+PKG_DEPENDS_TARGET="toolchain"
 PKG_SOURCE_DIR="FreeImage"
-PKG_DEPENDS_TARGET="toolchain libpng"
-PKG_SECTION="xmedia/games"
-PKG_SHORTDESC="FreeImage library"
-PKG_IS_ADDON="no"
-PKG_AUTORECONF="no"
-PKG_TOOLCHAIN="make"
+PKG_LONGDESC="FreeImage library"
 
-post_unpack() { 
- cp -f $PKG_DIR/dcraw_common.cpp $PKG_BUILD/Source/LibRawLite/internal/dcraw_common.cpp
-}  
-
-make_target() {
-  make DESTDIR=$SYSROOT_PREFIX 
+pre_make_target() {
+  export CXXFLAGS="$CXXFLAGS -Wno-narrowing"
+  export CFLAGS="$CFLAGS -DPNG_ARM_NEON_OPT=0"
 }
