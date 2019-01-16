a2b0516f4b3064ee4cb200851bf00732378f8fd1
Unknown <shantic@gmail.com>
2019-01-15 23:46:07 -0600

    Sx05RE: Move dependency to OEM package
    To keep CoreELEC clean

---
+++ packages/sx05re/Amlogic/package.mk
@@ -0,0 +1,14 @@
+################################################################################
+#      This file is part of sx05re
+#      no copyright, do as you please :)
+################################################################################
+
+PKG_NAME="Amlogic"
+PKG_VERSION=""
+PKG_LICENSE="various"
+PKG_SITE=""
+PKG_URL=""
+PKG_DEPENDS_TARGET="sx05re"
+PKG_SECTION="virtual"
+
+
