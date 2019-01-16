854c778367b297133aa40384d65f02cdbacf4090
Unknown <shantic@gmail.com>
2019-01-16 14:28:05 -0600

    libreto-beetle-pce-fast: bump to 433fb7

--- packages/sx05re/libretro-beetle-pce-fast/package.mk
+++ packages/sx05re/libretro-beetle-pce-fast/package.mk
@@ -1,38 +1,17 @@
-################################################################################
-#      This file is part of LibreELEC - http://www.libreelec.tv
-#      Copyright (C) 2016 Team LibreELEC
-#
-#  LibreELEC is free software: you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation, either version 2 of the License, or
-#  (at your option) any later version.
-#
-#  LibreELEC is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License
-#  along with LibreELEC.  If not, see <http://www.gnu.org/licenses/>.
-################################################################################
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="libretro-beetle-pce-fast"
-PKG_VERSION="39185a6"
-PKG_REV="1"
-PKG_ARCH="any"
-PKG_LICENSE="MAME"
+PKG_VERSION="433fb7a36fa26996a37f2107fb5189f243024f7d"
+PKG_SHA256="33d13fdbf315e4de9c04f6a70417a4afc25a82ca95f66f45e45818f1d3d37d6a"
+PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/libretro/beetle-pce-fast-libretro"
 PKG_URL="https://github.com/libretro/beetle-pce-fast-libretro/archive/$PKG_VERSION.tar.gz"
-PKG_SOURCE_DIR="beetle-pce-fast-libretro-$PKG_VERSION*"
 PKG_DEPENDS_TARGET="toolchain"
-PKG_SECTION="xmedia/games"
-PKG_SHORTDESC="libretro-beetle-pce-fast"
-PKG_LONGDESC="libretro-beetle-pce-fast"
-PKG_IS_ADDON="no"
-PKG_AUTORECONF="no"
+PKG_LONGDESC="libretro.beetle-pce-fast: Beetle PCE"
 
 make_target() {
-  make ARCH="" CC="$CC" NATIVE_CC="$CC" LD="$CC" -j 1
+  make
 }
 
 makeinstall_target() {
