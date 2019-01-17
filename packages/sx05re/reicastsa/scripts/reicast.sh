3578714ccd05cf571d74b71c7c990381b6ac80dd
Unknown <shantic@gmail.com>
2019-01-16 21:03:18 -0600

    ReicastSA: Try to automatically set the gamepad in emu.cfg

--- packages/sx05re/reicastsa/scripts/reicast.sh
+++ packages/sx05re/reicastsa/scripts/reicast.sh
@@ -12,6 +12,24 @@ mkdir -p /storage/.local/share/reicast/mappings
 cp -r /storage/.config/reicast/mappings/* /storage/.local/share/reicast/mappings
 fi
 
+
+# try to automatically set the gamepad in emu.cfg
+y=1
+
+
+for D in `find /dev/input/by-id/ | grep event-joystick`; do
+  str=$(ls -la $D)
+  i=$((${#str}-1))
+  DEVICE=$(echo "${str:$i:1}")
+  CFG="/storage/.config/reicast/emu.cfg"
+   sed -i -e "s/^evdev_device_id_$y =.*$/evdev_device_id_$y = $DEVICE/g" $CFG
+ # echo "Device: $y input $DEVICE"
+  y=$((y+1))
+ if [$y -lt 4]; then
+  break
+ fi 
+done
+
 /usr/bin/reicast "$1"
 
  
