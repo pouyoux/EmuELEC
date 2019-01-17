67870c63be50b4e9767bc059a2d5b65709e4f059
Unknown <shantic@gmail.com>
2019-01-17 14:42:54 -0600

    Libretro: rename libretro-beetle-pce-fast to avoid conflict with upstream

--- packages/sx05re/lr-beetle-pce-fast/package.mk
+++ packages/sx05re/lr-beetle-pce-fast/package.mk
@@ -1,7 +1,7 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
-PKG_NAME="libretro-beetle-pce-fast"
+PKG_NAME="lr-beetle-pce-fast"
 PKG_VERSION="433fb7a36fa26996a37f2107fb5189f243024f7d"
 PKG_SHA256="33d13fdbf315e4de9c04f6a70417a4afc25a82ca95f66f45e45818f1d3d37d6a"
 PKG_LICENSE="GPLv2"
