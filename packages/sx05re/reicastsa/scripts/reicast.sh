54640c97d4e810ae6b58852cc59aed096659c786
Unknown <shantic@gmail.com>
2019-01-17 01:49:37 -0600

    Reicastsa: cleanup

--- packages/sx05re/reicastsa/scripts/reicast.sh
+++ packages/sx05re/reicastsa/scripts/reicast.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
-#set reicast BIOS dir to point to /storage/roms/bios
+#set reicast BIOS dir to point to /storage/roms/bios/dc
 if [ ! -L /storage/.local/share/reicast/data ]; then
 	mkdir -p /storage/.local/share/reicast 
 	rm -rf /storage/.local/share/reicast/data
 	ln -s /storage/roms/bios/dc /storage/.local/share/reicast/data
 fi
 
-if [ ! -d /storage/.local/share/reicast/mappings ]; then
-mkdir -p /storage/.local/share/reicast/mappings
-cp -r /storage/.config/reicast/mappings/* /storage/.local/share/reicast/mappings
+if [ ! -L /storage/.local/share/reicast/mappings ]; then
+mkdir -p /storage/.local/share/reicast/
+ln -sf /storage/.config/reicast/mappings /storage/.local/share/reicast/mappings
 fi
 
 
