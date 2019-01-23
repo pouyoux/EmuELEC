c85666b6af437c473b3a203cf6bba2b0ca4020d9
Unknown <shantic@gmail.com>
2019-01-23 13:31:57 -0600

    Libretro: Bump some cores

--- packages/sx05re/libretro/fbalpha/package.mk
+++ packages/sx05re/libretro/fbalpha/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="fbalpha"
-PKG_VERSION="7e13ece"
+PKG_VERSION="b5fcd3f"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="Non-commercial"
