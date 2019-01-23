c8127cf969a63141be6f868465583acd87538fe3
Unknown <shantic@gmail.com>
2019-01-22 19:44:23 -0600

    Sx05RE: Remove duplicate package
    beetle-pce-fast is the same as beetle-pce

--- packages/sx05re/sx05re/package.mk
+++ packages/sx05re/sx05re/package.mk
@@ -19,7 +19,7 @@ PKG_AUTORECONF="no"
 PKG_TOOLCHAIN="make"
 
 # Thanks to magicseb  Reicast SA now WORKS :D
-PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET lr-beetle-pce-fast vba-next advancemame PPSSPPSDL reicastsa common-shaders scraper sx05re_frontend fbida"
+PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET advancemame PPSSPPSDL reicastsa common-shaders scraper sx05re_frontend fbida"
 
  
 # You can build some less used Libretro cores by using $LIBRETRO_EXTRA_CORES but you might run into size problems.
