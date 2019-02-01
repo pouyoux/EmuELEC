dff219319ac070be4a724bb8006229a63eb85f2e
Shanti Gilbert <shantic@gmail.com>
2019-02-01 11:02:10 -0600

    Retroarch: Bump to 0d3dc3f

--- packages/sx05re/libretro/retroarch/package.mk
+++ packages/sx05re/libretro/retroarch/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="retroarch"
-PKG_VERSION="f8ba41d"
+PKG_VERSION="0d3dc3f"
 PKG_LICENSE="GPLv3"
 PKG_SITE="https://github.com/libretro/RetroArch"
 PKG_URL="https://github.com/libretro/RetroArch.git"
