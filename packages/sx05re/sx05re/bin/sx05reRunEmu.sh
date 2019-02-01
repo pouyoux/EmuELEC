1447aefd20fd2aea7013252a0e699dafb494a0aa
Shanti Gilbert <shantic@gmail.com>
2019-02-01 11:01:20 -0600

    Sx05RE: Re-add the Splash screen
    removed by mistake

--- packages/sx05re/sx05re/bin/sx05reRunEmu.sh
+++ packages/sx05re/sx05re/bin/sx05reRunEmu.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 
+hdmimode=`cat /sys/class/display/mode`;
+
+# Set framebuffer geometry to match the resolution, splash should change according to the resolution. 
+case $hdmimode in
+  480*)            X=720  Y=480  SPLASH="/storage/.config/splash/splash-1080.png" ;;
+  576*)            X=720  Y=576  SPLASH="/storage/.config/splash/splash-1080.png" ;;
+  720p*)           X=1280 Y=720  SPLASH="/storage/.config/splash/splash-1080.png" ;;
+  *)               X=1920 Y=1080 SPLASH="/storage/.config/splash/splash-1080.png" ;;
+esac
+
 # Set the variables
 CFG="/storage/.emulationstation/es_settings.cfg"
 SX05RELOG="/storage/sx05re.log"
