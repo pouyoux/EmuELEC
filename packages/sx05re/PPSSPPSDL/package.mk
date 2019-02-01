a2ce14d68817fb073526597dae4a676d98543233
Shanti Gilbert <shantic@gmail.com>
2019-02-01 11:01:45 -0600

    PPSSPPSDL: bump to 4816e80

--- packages/sx05re/PPSSPPSDL/package.mk
+++ packages/sx05re/PPSSPPSDL/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="PPSSPPSDL"
-PKG_VERSION="ca6298e"
+PKG_VERSION="4816e80"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="MAME"
