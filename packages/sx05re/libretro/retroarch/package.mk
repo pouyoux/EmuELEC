f86508d01374e4d8e2122a5de3e256f46f982787
Shanti Gilbert <shantic@gmail.com>
2019-02-04 00:22:31 -0600

    Retroarch: bump to 12c6fe1 (v1.7.6)

--- packages/sx05re/libretro/retroarch/package.mk
+++ packages/sx05re/libretro/retroarch/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="retroarch"
-PKG_VERSION="d7410b6"
+PKG_VERSION="12c6fe1"
 PKG_LICENSE="GPLv3"
 PKG_SITE="https://github.com/libretro/RetroArch"
 PKG_URL="https://github.com/libretro/RetroArch.git"
