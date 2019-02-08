2458e865f871375fe79e2abd57fb4169efbbeb61
Shanti Gilbert <shantic@gmail.com>
2019-02-08 15:53:47 -0600

    Amiberry: Bump to f1f6486

--- packages/sx05re/amiberry/package.mk
+++ packages/sx05re/amiberry/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2018-present 5schatten (https://github.com/5schatten)
 
 PKG_NAME="amiberry"
-PKG_VERSION="2d3495c" # v2.24+
+PKG_VERSION="f1f6486" # v2.24+
 PKG_ARCH="arm"
 PKG_LICENSE="GPLv3"
 PKG_SITE="https://github.com/midwan/amiberry"
