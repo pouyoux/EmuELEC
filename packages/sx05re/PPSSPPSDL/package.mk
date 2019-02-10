51d99dc706c9e0897608b37a808d249ab0fb1b03
Shanti Gilbert <shantic@gmail.com>
2019-02-10 15:05:42 -0600

    PPSSPPSDL: Bump to e05a4c

--- packages/sx05re/PPSSPPSDL/package.mk
+++ packages/sx05re/PPSSPPSDL/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="PPSSPPSDL"
-PKG_VERSION="9736bc4"
+PKG_VERSION="e05a4ce"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="MAME"
