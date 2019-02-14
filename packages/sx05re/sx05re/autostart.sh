1b880fd62c68f3eea33a9c51cf43bb7ee21e231f
Shanti Gilbert <shantic@gmail.com>
2019-02-14 14:46:47 -0600

    Sx05RE: More cleanup

--- packages/sx05re/sx05re/autostart.sh
+++ packages/sx05re/sx05re/autostart.sh
@@ -9,13 +9,6 @@ mkdir -p /storage/.kodi/userdata/addon_data/service.coreelec.settings/
 cp /usr/config/nau/oe_settings.xml /storage/.kodi/userdata/addon_data/service.coreelec.settings/oe_settings.xml
 fi 
 
-# temp I need to find out why the retroarch.cfg is not being copied witouth this line
-# if [ ! -f "/storage/.config/retroarch/retroarch.cfg" ]; then
-# mkdir -p /storage/.config/retroarch
-# cp /etc/retroarch.cfg /storage/.config/retroarch/retroarch.cfg
-# fi 
-
-
 # Overclocking? 
 echo "performance" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
 echo "performance" > /sys/devices/system/cpu/cpu4/cpufreq/scaling_governor
