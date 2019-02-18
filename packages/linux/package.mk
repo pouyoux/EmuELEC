7ceb0e1523bae9a752007a684c2d5dee37d9abd4
adamg <adamg88@users.noreply.github.com>
2019-02-18 01:53:37 +0000

    linux: bump amlogic-3.14 to 974f821

--- packages/linux/package.mk
+++ packages/linux/package.mk
@@ -25,8 +25,8 @@ case "$LINUX" in
     PKG_BUILD_PERF="no"
     ;;
   amlogic-3.14)
-    PKG_VERSION="6f041639e769fb6d58d022bfa28e84f90ae4078a"
-    PKG_SHA256="3c8971d6eb31d46e7996f43cd40b8503aa816d79b8cde268087e2071320398c5"
+    PKG_VERSION="974f8215fe5ba8216653c13d517e81ae81463763"
+    PKG_SHA256="d269af83ab02fc026825f050abd3f458e5ca1c2aa1336a31316202ebb475b0c0"
     PKG_URL="https://github.com/CoreELEC/linux-amlogic/archive/$PKG_VERSION.tar.gz"
     PKG_SOURCE_NAME="linux-$LINUX-$PKG_VERSION.tar.gz"
     PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET aml-dtbtools:host"
