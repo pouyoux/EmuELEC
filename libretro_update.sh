4363604d2019f3f2a51c1c1d177f13cc6431358d
shantigilbert <shantic@gmail.com>
2019-02-22 17:56:54 -0600

    libretro_update.sh now also adds PKG_SHA256

--- libretro_update.sh
+++ libretro_update.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 
 # This file is part of the Lakka project and was created by ToKe79. It is originally from https://github.com/libretro/Lakka-LibreELEC/blob/master/libretro_update.sh
-# it has been sligtly modified to work with Sx05RE
+# it has been modified by Shanti Gilbert to work with Sx05RE
 
-[ -z "$LR_PKG_PATH" ] && LR_PKG_PATH="packages/sx05re/libretro"
+[ -z "$BUMPS" ] && BUMPS="no"
+[ -z "$LR_PKG_PATH" ] && LR_PKG_PATH="./packages"
 usage()
 {
   echo ""
   echo "$0 <--all [--exclude list] | --used [--exclude list] | --packages list>"
   echo ""
   echo "Updates PKG_VERSION in package.mk of libretro packages to latest."
-  echo ""
-  echo "Parameters:"
-  echo " -a --all                 Update all libretro packages"
-  echo " -u --used                Update only libretro packages used by Lakka"
   echo " -p list --packages list  Update listed libretro packages"
   echo " -e list --exclude list   Update all/used packages except listed ones"
   echo ""
@@ -34,7 +31,7 @@ case $1 in
           v="$@"
           [ "$v" == "" ] && { echo "Error: You must provide name(s) of package(s) to exclude after $x" ; exit 1 ; }
           for a in $v ; do
-            if [ -f $LR_PKG_PATH/$a/package.mk ] ; then
+            if [ -f $(find $LR_PKG_PATH -wholename */$a/package.mk) ] ; then
               PACKAGES_EX="$PACKAGES_EX $a"
             else
               echo "Warning: $a is not a libretro package."
@@ -63,7 +60,7 @@ case $1 in
           v="$@"
           [ "$v" == "" ] && { echo "Error: You must provide name(s) of package(s) to exclude after $x" ; exit 1 ; }
           for a in $v ; do
-            if [ -f $LR_PKG_PATH/$a/package.mk ] ; then
+            if [ -f $(find $LR_PKG_PATH -wholename */$a/package.mk) ] ; then
               PACKAGES_EX="$PACKAGES_EX $a"
             else
               echo "Warning: $a is not a libretro package."
@@ -77,14 +74,6 @@ case $1 in
           ;;
       esac
     fi
-    # Get list of cores, which are used with Lakka:
-    OPTIONS_FILE="distributions/Sx05RE/options"
-    [ -f "$OPTIONS_FILE" ] && source "$OPTIONS_FILE" || { echo "$OPTIONS_FILE: not found! Aborting." ; exit 1 ; }
-    [ -z "$LIBRETRO_CORES" ] && { echo "LIBRETRO_CORES: empty. Aborting!" ; exit 1 ; }
-    # List of core retroarch packages
-    RA_PACKAGES="retroarch retroarch-assets retroarch-joypad-autoconfig retroarch-overlays libretro-database core-info glsl-shaders"
-    # List of all libretro packages to update:
-    PACKAGES_ALL=" $RA_PACKAGES $LIBRETRO_CORES "
     ;;
   -p | --packages )
     PACKAGES_ALL=""
@@ -93,7 +82,7 @@ case $1 in
     v="$@"
     [ "$v" == "" ] && { echo "Error: You must provide name(s) of package(s) after $x" ; exit 1 ; }
     for a in $v ; do
-      if [ -f $LR_PKG_PATH/$a/package.mk ] ; then
+      if [ -f $(find $LR_PKG_PATH -wholename */$a/package.mk) ] ; then
         PACKAGES_ALL="$PACKAGES_ALL $a "
       else
         echo "Warning: $a is not a libretro package - skipping."
@@ -114,17 +103,19 @@ if [ "$PACKAGES_EX" != "" ] ; then
 fi
 echo "Checking following packages: "$PACKAGES_ALL
 declare -i i=0
+declare -i ii=0
 for p in $PACKAGES_ALL
 do
-  f=$LR_PKG_PATH/$p/package.mk
+  f=$(find $LR_PKG_PATH -wholename */$p/package.mk)
   if [ ! -f "$f" ] ; then
     echo "$f: not found! Skipping."
     continue
+    else
+    echo "working on : $f"
+    source config/options "$p"
+    source "$f"
   fi
-  PKG_VERSION=`cat $f | grep -oP 'PKG_VERSION="\K[^"]+'`
-  PKG_SITE=`cat $f | grep -oP 'PKG_SITE="\K[^"]+'`
-  PKG_NAME=`cat $f | grep -oP 'PKG_NAME="\K[^"]+'`
-  PKG_GIT_BRANCH=`cat $f | grep -oP 'PKG_GIT_BRANCH="\K[^"]+'`
+ 
   if [ -z "$PKG_VERSION" ] || [ -z "$PKG_SITE" ] ; then
     echo "$f: does not have PKG_VERSION or PKG_SITE"
     echo "PKG_VERSION: $PKG_VERSION"
@@ -132,15 +123,39 @@ do
     echo "Skipping update."
     continue
   fi
-  [ -n "$PKG_GIT_BRANCH" ] && GIT_HEAD="heads/$PKG_GIT_BRANCH" || GIT_HEAD="HEAD"
-  UPS_VERSION=`git ls-remote $PKG_SITE | grep ${GIT_HEAD}$ | awk '{ print substr($1,1,7) }'`
-  if [ "$UPS_VERSION" == "$PKG_VERSION" ]; then
-    echo "$PKG_NAME is up to date ($UPS_VERSION)"
-  else
-    i+=1
-    echo "$PKG_NAME updated from $PKG_VERSION to $UPS_VERSION"
-    sed -i "s/$PKG_VERSION/$UPS_VERSION/" $f
-  fi
-done
-echo "$i package(s) updated."
+     
+if [ $BUMPS != "no" ]; then
 
+  [ -n "$PKG_GIT_BRANCH" ] && GIT_HEAD="heads/$PKG_GIT_BRANCH" || GIT_HEAD="HEAD"
+   UPS_VERSION=`git ls-remote $PKG_SITE | grep ${GIT_HEAD}$ | awk '{ print substr($1,1,40) }'`
+   if [ "$UPS_VERSION" == "$PKG_VERSION" ]; then
+    echo "$PKG_NAME is up to date ($UPS_VERSION)"
+   else
+    i+=1
+     echo "$PKG_NAME updated from $PKG_VERSION to $UPS_VERSION"
+    sed -i "s/PKG_VERSION=\"$PKG_VERSION/PKG_VERSION=\"$UPS_VERSION/" $f
+   fi
+else
+  UPS_VERSION=$PKG_VERSION
+fi 
+
+  if [ "$GET_HANDLER_SUPPORT" != "git" ]; then  
+  
+   if grep -q PKG_SHA256 "$f"; then
+    echo "PKG_SHA256 exists on $f, clearing"
+    sed -i "s/PKG_SHA256=\"$PKG_SHA256\"/PKG_SHA256=\"\"/" $f
+    else
+    echo "PKG_SHA256 does not exists on $f, creating"
+    sed -i -e "s/PKG_VERSION=\"$UPS_VERSION\(.*\)\"/PKG_VERSION=\"$UPS_VERSION\1\"\nPKG_SHA256=\"\"/g" $f
+   fi
+
+    source "$f"
+    ./scripts/get "$PKG_NAME"
+    CALCSHA=$(cat ./sources/$PKG_NAME/$PKG_NAME-$UPS_VERSION.tar.gz.sha256)
+    echo "NEW SHA256 $CALCSHA"
+    sed -i "s/PKG_SHA256=\"$PKG_SHA256/PKG_SHA256=\"$CALCSHA/" $f
+    ii+=1
+ fi
+    
+ done
+echo "$i package(s) bumped. $ii sha256 updated packages"
