82c842e8d326430600208c27ba52a83c8289ea70
shantigilbert <shantic@gmail.com>
2019-03-08 17:28:51 -0600

    FBIDA: Stop using precompiled binaries
    Thanks to Vpeter!

--- packages/sx05re/tools/fbida/package.mk
+++ packages/sx05re/tools/fbida/package.mk
@@ -1,46 +1,28 @@
-################################################################################
-#      This file is part of OpenELEC - http://www.openelec.tv
-#      Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
-#
-#  OpenELEC is free software: you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation, either version 2 of the License, or
-#  (at your option) any later version.
-#
-#  OpenELEC is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License
-#  along with OpenELEC.  If not, see <http://www.gnu.org/licenses/>.
-################################################################################
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2018-present CoreELEC (https://coreelec.org)
 
 PKG_NAME="fbida"
-PKG_VERSION="2.13"
+PKG_VERSION="b733a22190c70443fedcd4e87397c426c9d48af6"
+PKG_SHA256="4aa43acc5c47fac8cb54db787d7a77ad4c20c08e2913162cf52a53ab9533740f"
 PKG_ARCH="any"
-PKG_LICENSE="GPL"
-PKG_SITE=""
-PKG_URL=""
-PKG_DEPENDS_TARGET="toolchain"
-PKG_SECTION="tools"
+PKG_SITE="https://github.com/kraxel/fbida"
+PKG_URL="$PKG_SITE/archive/$PKG_VERSION.tar.gz"
+PKG_DEPENDS_TARGET="toolchain libexif pixman poppler libdrm libepoxy tiff"
 PKG_SHORTDESC="The fbida project contains a few applications for viewing and editing images, with the main focus being photos."
 PKG_LONGDESC="The fbida project contains a few applications for viewing and editing images, with the main focus being photos."
+PKG_TOOLCHAIN="manual"
 
-PKG_IS_ADDON="no"
-PKG_AUTORECONF="no"
-PKG_TOOLCHAIN="make"
+pre_configure_target() {
+  sed -i "s|cpp -include jpeglib.h|$CPP -include $SYSROOT_PREFIX/usr/include/jpeglib.h|" GNUmakefile
+
+  CFLAGS="$CFLAGS -I$(get_build_dir libepoxy)/include"
+}
 
 make_target() {
-  : not
+  make -f GNUmakefile fbi
 }
 
 makeinstall_target() {
-
-  mkdir -p $INSTALL/usr/lib/
-    cp -rf $PKG_DIR/lib/* $INSTALL/usr/lib/
-   
-  mkdir -p $INSTALL/usr/bin/
-    cp -rf $PKG_DIR/bin/* $INSTALL/usr/bin/
-    chmod +x $INSTALL/usr/bin/fbi
+  mkdir -p $INSTALL/usr/bin
+    cp fbi $INSTALL/usr/bin
 }
