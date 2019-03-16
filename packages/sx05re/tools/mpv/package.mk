74efaef6088aba7c653108f1a15cec9f63adc2b8
shantigilbert <shantic@gmail.com>
2019-03-16 12:55:21 -0600

    mpv: more cleanup

--- packages/sx05re/tools/mpv/package.mk
+++ packages/sx05re/tools/mpv/package.mk
@@ -4,18 +4,17 @@
 
 PKG_NAME="mpv"
 PKG_VERSION="dd83b66652d93e5422757f569b084867a9052e48"
-PKG_SHA256=""
-PKG_LICENSE="GPLv3+"
+PKG_SHA256="8560e948e257a1e301a3a4e4bf2628436db28072791107f1edbced043b29717f"
+PKG_LICENSE="GPLv2+"
 PKG_SITE="https://github.com/mpv-player/mpv"
 PKG_URL="$PKG_SITE/archive/$PKG_VERSION.tar.gz"
-PKG_DEPENDS_TARGET="toolchain Python2 SDL2-git"
+PKG_DEPENDS_TARGET="toolchain SDL2-git"
 PKG_LONGDESC="Video player based on MPlayer/mplayer2 https://mpv.io"
-PKG_BUILD_FLAGS="-gold"
 PKG_TOOLCHAIN="manual"
 
 configure_target() {
   ./bootstrap.py
-  ./waf configure --enable-mali-fbdev --enable-sdl2 --disable-pulse --disable-libbluray
+  ./waf configure --enable-sdl2 --disable-pulse --disable-libbluray
 }
 
 make_target() {
@@ -23,7 +22,6 @@ make_target() {
 }
 
 makeinstall_target() {
-  #./waf install --destdir=$INSTALL --targets=mpv -j$CONCURRENCY_MAKE_LEVEL
   mkdir -p $INSTALL/usr/bin
   cp ./build/mpv $INSTALL/usr/bin
 }
