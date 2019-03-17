91a73a722afb6b440bfba57803dd72f6c4a04db3
shantigilbert <shantic@gmail.com>
2019-03-17 16:08:44 -0600

    Sx05RE: Fix joypad mount error

--- packages/sx05re/sx05re/package.mk
+++ packages/sx05re/sx05re/package.mk
@@ -80,6 +80,8 @@ rm -rf "$INSTALL/usr/share/retroarch-overlays/ipad"
 rm -rf "$INSTALL/usr/share/retroarch-overlays/keyboards"
 rm -rf "$INSTALL/usr/share/retroarch-overlays/misc"
 
+mkdir -p $INSTALL/etc/retroarch-joypad-autoconfig
+
   # update addon manifest
    ADDON_MANIFEST=$INSTALL/usr/share/kodi/system/addon-manifest.xml
    xmlstarlet ed -L --subnode "/addons" -t elem -n "addon" -v "script.emulationstation.launcher" $ADDON_MANIFEST
