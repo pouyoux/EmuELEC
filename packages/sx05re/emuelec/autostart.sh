1d6009e9287939714f690a80d6e30e6736eacb69
shantigilbert <shantic@gmail.com>
2019-03-26 23:57:08 -0600

    ES: create folder sooner and some BGM fixes

--- packages/sx05re/emuelec/autostart.sh
+++ packages/sx05re/emuelec/autostart.sh
@@ -1,34 +1,42 @@
 #!/bin/sh
 
+# It seems some slow SDcards have a problem creating the symlink on time :/
+CONFIG_DIR="/storage/.emulationstation"
+CONFIG_DIR2="/storage/.config/emulationstation"
+
+if [ ! -L "$CONFIG_DIR" ]; then
+ln -sf $CONFIG_DIR2 $CONFIG_DIR
+fi
+
 # Clean cache garbage when boot up.
 rm -rf /storage/.cache/cores/*
 
 # Overclocking? 
 echo "performance" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
-echo "performance" > /sys/devices/system/cpu/cpu4/cpufreq/scaling_governor
+# echo "performance" > /sys/devices/system/cpu/cpu4/cpufreq/scaling_governor
 echo 5 > /sys/class/mpgpu/cur_freq
 
 # Is BMG enabled ? only after the first boot
 if [ -f /storage/.emulationstation/es_settings.cfg ]; then
 
-DEFE=$(sed -n 's|\s*<string name="EmuELEC_BGM_BOOT" value="\(.*\)" />|\1|p' /storage/.emulationstation/es_settings.cfg)
+DEFE=$(sed -n 's|\s*<string name="EmuELEC_BGM_BOOT" value="\(.*\)" />|\1|p' $CONFIG_DIR/es_settings.cfg)
 
 case "$DEFE" in
 "Yes")
 	killall mpg123
 	/storage/.emulationstation/scripts/bgm.sh
-	sed -i -e "s/name=\"BGM\" value=\"false\"/name=\"BGM\" value=\"true\"/" /storage/.emulationstation/es_settings.cfg
+	sed -i -e "s/name=\"BGM\" value=\"false\"/name=\"BGM\" value=\"true\"/" $CONFIG_DIR/es_settings.cfg
 	;;
-*)
+"No")
 	killall mpg123
-	sed -i -e "s/name=\"BGM\" value=\"true\"/name=\"BGM\" value=\"false\"/" /storage/.emulationstation/es_settings.cfg
+	sed -i -e "s/name=\"BGM\" value=\"true\"/name=\"BGM\" value=\"false\"/" $CONFIG_DIR/es_settings.cfg
 	;;
 esac
 
 fi
 
 # What to start at boot?
-DEFE=$(sed -n 's|\s*<string name="EmuELEC_BOOT" value="\(.*\)" />|\1|p' /storage/.emulationstation/es_settings.cfg)
+DEFE=$(sed -n 's|\s*<string name="EmuELEC_BOOT" value="\(.*\)" />|\1|p' $CONFIG_DIR/es_settings.cfg)
 
 case "$DEFE" in
 "Retroarch")
@@ -48,6 +56,7 @@ case "$DEFE" in
 	/usr/bin/startfe.sh &
 	;;
 esac
+
 
 
 # Bluetooth, Make sure you change your BT MAC address, you need to do this by SSH the first time
