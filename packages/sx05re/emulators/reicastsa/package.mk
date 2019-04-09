cb73c8f01206c3220ad8f24a88613554eb44a34b
Shanti Gilbert <shantic@gmail.com>
2019-04-08 23:40:40 -0500

    Revert "Reicastsa: bump to 7e11e7a"
    This reverts commit 28cf9312592ca3dc41d6f68f8fa62eb19ab599bb.

--- packages/sx05re/emulators/reicastsa/package.mk
+++ packages/sx05re/emulators/reicastsa/package.mk
@@ -17,21 +17,21 @@
 ################################################################################
 
 PKG_NAME="reicastsa"
-PKG_VERSION="7e11e7aff6d704de4ad8ad7531f597df058099ac"
-PKG_SHA256="07978933e040470b1fbb8c887485703f10fb828b583b350e23e834e7b8cee4bc"
+PKG_VERSION="cb278e367b5e5635be9ebf45fd77fac2ce2fed7a"
+PKG_SHA256="74f69c7b1122b178a17840b51a225b8487dfc8bc30dacc9153495e0c88683259"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/reicast/reicast-emulator"
 PKG_URL="https://github.com/reicast/reicast-emulator/archive/$PKG_VERSION.tar.gz"
 PKG_SOURCE_DIR="reicast-emulator-$PKG_VERSION*"
-PKG_DEPENDS_TARGET="toolchain alsa libpng libevdev libX11 python-evdev pulseaudio"
+PKG_DEPENDS_TARGET="toolchain alsa-utils libpng libevdev libX11 python-evdev"
 PKG_SHORTDESC="Reicast is a multi-platform Sega Dreamcast emulator"
 PKG_TOOLCHAIN="make"
 PKG_BUILD_FLAGS="-gold"
 
 make_target() {
   cd shell/linux
-      make CC=$CC CXX=$CXX AS=$CC STRIP=$STRIP platform=odroidc2 reicast.elf USE_PULSEAUDIO=1
+      make CC=$CC CXX=$CXX AS=$CC STRIP=$STRIP platform=odroidc2 reicast.elf
 }
 
 makeinstall_target() {
