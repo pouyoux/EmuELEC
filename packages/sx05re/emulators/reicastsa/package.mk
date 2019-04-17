b55c50120a2bd478f9abedb112b91ecd0bff068d
Shanti Gilbert <shantic@gmail.com>
2019-04-17 00:29:15 -0500

    Use  version of Reicast according to project

--- packages/sx05re/emulators/reicastsa/package.mk
+++ packages/sx05re/emulators/reicastsa/package.mk
@@ -1,33 +1,28 @@
 ################################################################################
-#      This file is part of LibreELEC - http://www.libreelec.tv
-#      Copyright (C) 2016 Team LibreELEC
-#
-#  LibreELEC is free software: you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation, either version 2 of the License, or
-#  (at your option) any later version.
-#
-#  LibreELEC is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License
-#  along with LibreELEC.  If not, see <http://www.gnu.org/licenses/>.
+#      This file is part of emuelec
+#      no copyright, do as you please :)
 ################################################################################
 
 PKG_NAME="reicastsa"
+if [ "${PROJECT}" == "Amlogic-ng" ]; then
 PKG_VERSION="7e11e7aff6d704de4ad8ad7531f597df058099ac"
 PKG_SHA256="07978933e040470b1fbb8c887485703f10fb828b583b350e23e834e7b8cee4bc"
+else
+PKG_VERSION="cb278e367b5e5635be9ebf45fd77fac2ce2fed7a"
+PKG_SHA256="74f69c7b1122b178a17840b51a225b8487dfc8bc30dacc9153495e0c88683259"
+fi
+
 PKG_ARCH="any"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/reicast/reicast-emulator"
 PKG_URL="https://github.com/reicast/reicast-emulator/archive/$PKG_VERSION.tar.gz"
 PKG_SOURCE_DIR="reicast-emulator-$PKG_VERSION*"
-PKG_DEPENDS_TARGET="toolchain alsa libpng libevdev libX11 python-evdev"
+PKG_DEPENDS_TARGET="toolchain alsa libpng libevdev python-evdev"
 PKG_SHORTDESC="Reicast is a multi-platform Sega Dreamcast emulator"
 PKG_TOOLCHAIN="make"
 PKG_BUILD_FLAGS="-gold"
+
+PKG_PATCH_DIRS="${PROJECT}"
 
 make_target() {
   cd shell/linux
