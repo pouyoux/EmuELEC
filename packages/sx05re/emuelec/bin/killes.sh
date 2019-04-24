25db71ae945d85e39f451f19f8cf2eb03758e57f
Shanti Gilbert <shantic@gmail.com>
2019-04-24 17:40:52 -0500

    remove kodi traces

--- packages/sx05re/emuelec/bin/killes.sh
+++ packages/sx05re/emuelec/bin/killes.sh
@@ -1,11 +1,8 @@
 #!/bin/sh
 
-    rm /var/lock/start.kodi
-    systemctl stop kodi
-  
  if pgrep -x "/usr/bin/emulationstation" > /dev/null
   then
     killall -9 emulationstation
   fi
-  
+
  exit 0
