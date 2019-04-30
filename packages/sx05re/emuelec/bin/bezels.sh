3aef3c4a368458727dabc3534b1ef68919b13a5c
Shanti Gilbert <shantic@gmail.com>
2019-04-29 22:12:05 -0500

    emuELEC: auto Bezels and Splash WIP

---
+++ packages/sx05re/emuelec/bin/bezels.sh
@@ -0,0 +1,172 @@
+#!/bin/sh
+
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2019-present SumavisionQ5 (https://github.com/SumavisionQ5)
+# Modifications by Shanti Gilbert (https://github.com/shantigilbert)
+
+#ZIPDIR="$2"
+#ROMNAME="${2##*/}"
+#ROMNAME="${ROMNAME%.*}"
+ROMNAME=$(basename ${2%.*})
+RACONFIG="/storage/.config/retroarch/retroarch.cfg"
+OPACITY="1.000000"
+AR_INDEX="23"
+BEZELDIR="/storage/overlays/bezels"
+INIFILE="$BEZELDIR/setting.ini"
+
+# bezelmap.cfg in $BEZELDIR/ is to share bezels between arcade clones and parent. 
+BEZELMAP="$BEZELDIR/bezelmap.cfg"
+BZLNAME=$(sed -n "/"$1"_"$ROMNAME" = /p" "$BEZELMAP")
+BZLNAME="${BZLNAME#*\"}"
+BZLNAME="${BZLNAME%\"*}"
+OVERLAYDIR1="$BEZELDIR/$1/$ROMNAME.cfg"
+OVERLAYDIR2="$BEZELDIR/$1/$BZLNAME.cfg"
+
+sed -i '/input_overlay_opacity/d' $RACONFIG
+sed -i "\$i input_overlay_opacity = \"${OPACITY}\"\n" $RACONFIG
+
+clear_bezel() { 
+		sed -i '/aspect_ratio_index = "/d' $RACONFIG
+		sed -i '/custom_viewport_width = "/d' $RACONFIG
+		sed -i '/custom_viewport_height = "/d' $RACONFIG
+		sed -i '/custom_viewport_x = "/d' $RACONFIG
+		sed -i '/custom_viewport_y = "/d' $RACONFIG
+		sed -i '/video_scale_integer = "/d' $RACONFIG
+		sed -i 'video_scale_integer = "false"' $RACONFIG
+	}
+
+set_bezel() {
+# $OPACITY: input_overlay_opacity
+# $AR_INDEX: aspect_ratio_index
+# $1: custom_viewport_width 
+# $2: custom_viewport_height
+# $3: ustom_viewport_x
+# $4: custom_viewport_y
+# $5: video_scale_integer
+# $6: video_scale 
+        
+        clear_bezel
+        sed -i "1i input_overlay_opacity = \"$OPACITY\"" $RACONFIG
+		sed -i "2i aspect_ratio_index = \"$AR_INDEX\"" $RACONFIG
+		sed -i "3i custom_viewport_width = \"$1\"" $RACONFIG
+		sed -i "4i custom_viewport_height = \"$2\"" $RACONFIG
+		sed -i "5i custom_viewport_x = \"$3\"" $RACONFIG
+		sed -i "6i custom_viewport_y = \"$4\"" $RACONFIG
+		sed -i "7i video_scale_integer = \"$5\"" $RACONFIG
+	}
+
+check_overlay_dir() {
+
+# The bezel will be searched and used in following order:
+# 1.$OVERLAYDIR1 will be used, if it does not exist, then
+# 2.$OVERLAYDIR2 will be used, if it does not exist, then
+# 3.default bezel as "$BEZELDIR/$1/default.cfg\" will be used.
+
+	if [ -f $OVERLAYDIR1 ]; then
+		sed -i '/input_overlay = "/d' $RACONFIG
+		echo -e "input_overlay = \""$OVERLAYDIR1"\"\n" >> $RACONFIG
+	elif [ -f $OVERLAYDIR2 ]; then
+		sed -i '/input_overlay = "/d' $RACONFIG
+		echo -e "input_overlay = \""$OVERLAYDIR2"\"\n" >> $RACONFIG
+	else
+		sed -i '/input_overlay = "/d' $RACONFIG
+		echo -e "input_overlay = \"$BEZELDIR/$1/default.cfg\"\n" >> $RACONFIG
+	fi
+}
+
+# Only 720P and 1080P can use bezels. For 480p/i and 576p/i we just delete bezel config.
+hdmimode=$(`cat /sys/class/display/mode`)
+
+case $hdmimode in
+  480*)
+	sed -i '/input_overlay = "/d' $RACONFIG
+  ;;
+  576*)
+	sed -i '/input_overlay = "/d' $RACONFIG
+  ;;
+  720p*)
+	
+	check_overlay_dir $1
+	case "$1" in
+   "GBA")
+		set_bezel "467" "316" "405" "190" "false"
+		;;
+	"GG")
+		set_bezel "780" "580" "245" "70" "false"
+		;;
+	"GB")
+		set_bezel "429" "380" "420" "155" "false"
+		;;
+	"GBC")
+		set_bezel "430" "380" "425" "155" "false"
+		;;
+	"NGP")
+		set_bezel "461" "428" "407" "145" "false"
+		;;
+    "NGPC")
+		set_bezel "460" "428" "407" "145" "false"
+		;;
+	"WS")
+		set_bezel "645" "407" "325" "150" "false"
+		;;
+	"WSC")
+		set_bezel "643" "405" "325" "150" "false"
+		;;
+	*)
+		# delete aspect_ratio_index to make sure video is expanded fullscreen. Only certain handheld platforms need custom_viewport.
+		clear_bezel
+		;;
+	esac
+  ;;
+  # For Amlogic TV box, the following resolution is 1080p/i.
+  *)
+    check_overlay_dir $1
+	case "$1" in
+   "GBA")
+		set_bezel "960" "640" "0" "0" "true"
+		;;
+	"GG")
+		set_bezel "1160" "850" "380" "120" "false"
+		;;
+	"GB")
+		set_bezel "625" "565" "645" "235" "false"
+		;;
+	"GBC")
+		set_bezel "625" "565" "645" "235" "false"
+		;;
+	"NGP")
+		set_bezel "700" "635" "610" "220" "false"
+		;;
+	"NGPC")
+		set_bezel "700" "640" "610" "215" "false"
+		;;
+	"WS")
+		set_bezel "950" "605" "490" "225" "false"
+		;;
+	"WSC")
+		set_bezel "950" "605" "490" "225" "false"
+		;;
+	*)
+		clear_bezel
+		;;
+	esac
+  ;;
+esac
+
+# If we disable bezel in setting.ini for certain platform, we just delete bezel config.
+Bezel=$(sed -n "/"$1"_Bezel = /p" $INIFILE)
+Bezel="${Bezel#*\"}"
+Bezel="${Bezel%\"*}"
+if [ "$Bezel" = "OFF" ]; then
+sed -i '/input_overlay = "/d' $RACONFIG
+fi
+
+# Note:
+# 1. Different handheld platforms have different bezels, they may need different viewport value even for same platform.
+#	So, I think this script should be stored in $BEZELDIR/ or some place wich can be modified by users.
+# 2. For Arcade games, I created a bezelmap.cfg in $BEZELDIR/ in order to share bezels between arcade clones and parent. 
+#	In fact, ROMs of other platforms can share certain bezel if you write mapping relationship in bezelmap.cfg.
+# 3. I modified es_systems.cfg to set $1 as platfrom for all platfrom.
+#	For some libretro core such as <command>/usr/bin/sx05reRunEmu.sh LIBRETRO scummvm %ROM%</command>, $1 not right platform value,
+#	you may need some tunings on them.
+# 4. I am a Linux noob, so the codes are a mess. Sorry for that:)
--- packages/sx05re/emuelec/bin/show_splash.sh
+++ packages/sx05re/emuelec/bin/show_splash.sh
@@ -1,18 +1,39 @@
 #!/bin/sh
 
-hdmimode=`cat /sys/class/display/mode`;
-case $hdmimode in
-  480*)            X=720  Y=480  SPLASH="/storage/.config/splash/loading-game.png" ;;
-  576*)            X=720  Y=576  SPLASH="/storage/.config/splash/loading-game.png" ;;
-  720p*)           X=1280 Y=720  SPLASH="/storage/.config/splash/loading-game.png" ;;
-  *)               X=1920 Y=1080 SPLASH="/storage/.config/splash/loading-game.png" ;;
-esac
+if [ "$1" == "intro" ]; then
+	SPLASH="/storage/.config/splash/splash-1080.png"
+else
+	SPLASHDIR="/storage/overlays/splash"
+	#ZIPDIR="$2"
+	#ROMNAME="${2##*/}"
+	#ROMNAME="${ROMNAME%.*}"
+	ROMNAME=$(basename ${2%.*})
+	SPLMAP="$SPLASHDIR/splashmap.cfg"
+	SPLNAME=$(sed -n "/`echo ""$1"_"${ROMNAME}" = "`/p" "$SPLMAP")
+	REALSPL="${SPLNAME#*\"}"
+	REALSPL="${REALSPL%\"*}"
+	SPLASH1="$SPLASHDIR/$1/$REALSPL.png"
+	SPLASH2="$SPLASHDIR/$1/$ROMNAME.png"
+	SPLASH3="$SPLASHDIR/$1/splash.png"
+if [ -f $SPLASH1 ]; then
+	SPLASH=$SPLASH1
+elif [ -f $SPLASH2 ]; then
+	SPLASH=$SPLASH2
+elif [ -f $SPLASH3 ]; then
+	SPLASH=$SPLASH3
+else
+	SPLASH="/storage/.config/splash/splash-1080.png"
+fi
 
+# hdmimode=`cat /sys/class/display/mode`;
+# case $hdmimode in
+#  480*)            X=720  Y=480  SPLASH="/storage/.config/splash/loading-game.png" ;;
+#  576*)            X=720  Y=576  SPLASH="/storage/.config/splash/loading-game.png" ;;
+#  720p*)           X=1280 Y=720  SPLASH="/storage/.config/splash/loading-game.png" ;;
+#  *)               X=1920 Y=1080 SPLASH="/storage/.config/splash/loading-game.png" ;;
+# esac
 
-SPLASH="/storage/.config/splash/loading-game.png"
-
-if "$1" == "intro"; then
-SPLASH="/storage/.config/splash/splash-1080.png"
+#SPLASH="/storage/.config/splash/loading-game.png"
 fi 
 
 (
