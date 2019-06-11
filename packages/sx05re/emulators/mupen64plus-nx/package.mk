438df6db313c7f0f43bd40079f114cd264ff97eb
Shanti Gilbert <shantic@gmail.com>
2019-06-11 16:35:29 -0500

    Mupen64plus-nx: Bump to 96608ba

--- packages/sx05re/emulators/mupen64plus-nx/package.mk
+++ packages/sx05re/emulators/mupen64plus-nx/package.mk
@@ -2,56 +2,26 @@
 # Copyright (C) 2019-present Shanti Gilbert (https://github.com/shantigilbert)
 
 PKG_NAME="mupen64plus-nx"
-PKG_VERSION="1c2029a"
+PKG_VERSION="96608bace2b9caf3fd5f7e685b2d3ce2add2ff0e"
+PKG_SHA256="c208e4b33887635dcace82f6af9b6a81bfd9e305e845a0880242b44862cc6f53"
 PKG_REV="1"
-PKG_ARCH="arm i386 x86_64"
+PKG_ARCH="arm"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/libretro/mupen64plus-libretro-nx"
 PKG_URL="$PKG_SITE/archive/$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain nasm:host $OPENGLES"
-PKG_PRIORITY="optional"
 PKG_SECTION="libretro"
 PKG_SHORTDESC="mupen64plus + RSP-HLE + GLideN64 + libretro"
 PKG_LONGDESC="mupen64plus + RSP-HLE + GLideN64 + libretro"
-
-PKG_IS_ADDON="no"
 PKG_TOOLCHAIN="make"
-PKG_AUTORECONF="no"
 PKG_BUILD_FLAGS="-lto"
 
-make_target() {
-  case $PROJECT in
-    RPi|Gamegirl|Slice)
-      CFLAGS="$CFLAGS -I$SYSROOT_PREFIX/usr/include/interface/vcos/pthreads \
-	              -I$SYSROOT_PREFIX/usr/include/interface/vmcs_host/linux"
-      make platform=rpi GLES=1 FORCE_GLES=1 WITH_DYNAREC=arm
-      ;;
-    RPi2|Slice3)
-      CFLAGS="$CFLAGS -I$SYSROOT_PREFIX/usr/include/interface/vcos/pthreads \
-                      -I$SYSROOT_PREFIX/usr/include/interface/vmcs_host/linux"
-      make platform=rpi2 GLES=1 FORCE_GLES=1 HAVE_NEON=1 WITH_DYNAREC=arm
-      ;;
-    imx6|Amlogic*)
-      CFLAGS="$CFLAGS -DLINUX -DEGL_API_FB"
-      CPPFLAGS="$CPPFLAGS -DLINUX -DEGL_API_FB"
-      make platform=unix GLES=1 FORCE_GLES=1 HAVE_NEON=1 WITH_DYNAREC=arm
-      ;;
-    Generic)
-      make
-      ;;
-    OdroidC1)
-      make platform=odroid BOARD=ODROID-C1 GLES=1 FORCE_GLES=1 HAVE_NEON=1 WITH_DYNAREC=arm
-      ;;
-    OdroidXU3)
-      make platform=odroid BOARD=ODROID-XU3 GLES=1 FORCE_GLES=1 HAVE_NEON=1 WITH_DYNAREC=arm
-      ;;
-    ROCK960)
-      make platform=unix-gles GLES=1 FORCE_GLES=1 HAVE_NEON=1 WITH_DYNAREC=arm
-      ;;
-    *)
-      make platform=unix-gles GLES=1 FORCE_GLES=1 HAVE_NEON=1 WITH_DYNAREC=arm
-      ;;
-  esac
+PKG_MAKE_OPTS_TARGET+=" platform=amlogic GLES=1 FORCE_GLES=1 HAVE_NEON=1 WITH_DYNAREC=arm"
+
+pre_make_target() { 
+if [ ${PROJECT} = "Amlogic-ng" ]; then
+	sed -i "s|-mcpu=cortex-a53 -mtune=cortex-a53|-mcpu=cortex-a73 -mtune=cortex-a73.cortex-a53|g" $PKG_BUILD/Makefile
+fi 
 }
 
 makeinstall_target() {
