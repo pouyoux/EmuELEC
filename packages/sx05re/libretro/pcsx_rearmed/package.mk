fb13635ab8705d317cc4cf45f306fec844b838c4
Shanti Gilbert <shantic@gmail.com>
2019-07-05 17:51:02 -0500

    pcsx_rearmed: Use proper compilation flags

--- packages/sx05re/libretro/pcsx_rearmed/package.mk
+++ packages/sx05re/libretro/pcsx_rearmed/package.mk
@@ -19,8 +19,8 @@
 ################################################################################
 
 PKG_NAME="pcsx_rearmed"
-PKG_VERSION="a3c2efcd073d986f17a4a81617acaf68e07447e1"
-PKG_SHA256="0fdfa130a9a6f7da050d310d1b0299ff0c165bd096d758a8fbd4a1a04e97e444"
+PKG_VERSION="d440c93538a8f18076505e3ea33c6f6bb5c20dc0"
+PKG_SHA256="eaea147e78c762386f7a2e777864707a9b4a3ac87363d8ce2deeb990339caa06"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv2"
@@ -31,22 +31,20 @@ PKG_PRIORITY="optional"
 PKG_SECTION="libretro"
 PKG_SHORTDESC="ARM optimized PCSX fork"
 PKG_LONGDESC="PCSX ReARMed is yet another PCSX fork based on the PCSX-Reloaded project, which itself contains code from PCSX, PCSX-df and PCSX-Revolution."
-
 PKG_IS_ADDON="no"
 PKG_TOOLCHAIN="make"
 PKG_AUTORECONF="no"
-
 PKG_BUILD_FLAGS="-gold"
 
-make_target() {
-  cd $PKG_BUILD
-  if [[ "$TARGET_FPU" =~ "neon" ]]; then
-    make -f Makefile.libretro HAVE_NEON=1 USE_DYNAREC=1 BUILTIN_GPU=neon
-  elif [ "$ARCH" == "arm" ]; then
-    make -f Makefile.libretro HAVE_NEON=0 USE_DYNAREC=1
+PKG_MAKE_OPTS_TARGET="-f Makefile.libretro GIT_VERSION=${PKG_VERSION:0:7}"
+
+pre_configure_target() {
+ cd ${PKG_BUILD}
+  if [ "${PROJECT}" = "Amlogic-ng" ]; then
+    PKG_MAKE_OPTS_TARGET+=" platform=rpi4"
   else
-    make -f Makefile.libretro
-  fi
+    PKG_MAKE_OPTS_TARGET+=" platform=rpi3"
+    fi
 }
 
 makeinstall_target() {
